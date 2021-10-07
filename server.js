157344146c47b10db23e3c4622d59677cd20718a
mosesejim <mosesejim@outlook.com>
2021-10-07 08:41:25 +0100

    setting up for deployment

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.get('/', function(req, res) {
-    res.json({ msg: 'BuyRequest works' });
+    res.json({ msg: 'Welcome to my Phone stores' });
 });
 
 // DB config
